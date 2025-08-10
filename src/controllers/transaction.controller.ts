722a217c20666715bb3fa576f15192b4a076e94b
Prince Nmezi <stephenprince427@gmail.com>
2025-08-10 02:06:00 +0100

    added more transaction endpoint(duplicate transaction and pagination)

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from "express";
 import { asyncHandler } from "../middlewares/asyncHandler.middleware";
-import { createTransactionSchema } from "../validators/transaction.validator";
-import { createTransactionService } from "../services/transaction.service";
+import { createTransactionSchema, transactionIdSchema } from "../validators/transaction.validator";
+import { createTransactionService, duplicateTransactionService, getAllTransactionService, getTransactionByIdService } from "../services/transaction.service";
 import { HTTPSTATUS } from "../utils/http_codes";
+import { TransactionTypeEnum } from "../models/transaction.model";
 
 
 export const createTransactionController = asyncHandler(
@@ -15,6 +16,65 @@ export const createTransactionController = asyncHandler(
     return res.status(HTTPSTATUS.CREATED).json({
       message: "Transacton created successfully",
       transaction,
+    });
+  }
+);
+
+
+export const getAllTransactionController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user?._id;
+
+    const filters = {
+      keyword: req.query.keyword as string | undefined,
+      type: req.query.type as keyof typeof TransactionTypeEnum | undefined,
+      recurringStatus: req.query.recurringStatus as
+        | "RECURRING"
+        | "NON_RECURRING"
+        | undefined,
+    };
+
+    const pagination = {
+      pageSize: parseInt(req.query.pageSize as string) || 20,
+      pageNumber: parseInt(req.query.pageNumber as string) || 1,
+    };
+
+    const result = await getAllTransactionService(userId, filters, pagination);
+
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Transaction fetched successfully",
+      ...result,
+    });
+  }
+);
+
+export const getTransactionByIdController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user?._id;
+    const transactionId = transactionIdSchema.parse(req.params.id);
+
+    const transaction = await getTransactionByIdService(userId, transactionId);
+
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Transaction fetched successfully",
+      transaction,
+    });
+  }
+);
+
+export const duplicateTransactionController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const userId = req.user?._id;
+    const transactionId = transactionIdSchema.parse(req.params.id);
+
+    const transaction = await duplicateTransactionService(
+      userId,
+      transactionId
+    );
+
+    return res.status(HTTPSTATUS.OK).json({
+      message: "Transaction duplicated successfully",
+      data: transaction,
     });
   }
 );
--- src/routes/transaction.route.ts
+++ src/routes/transaction.route.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { createTransactionController } from "../controllers/transaction.controller";
+import {
+  createTransactionController,
+  duplicateTransactionController,
+  getAllTransactionController,
+  getTransactionByIdController,
+} from "../controllers/transaction.controller";
 
 const transactionRoutes = Router();
 
 transactionRoutes.post("/create", createTransactionController);
+transactionRoutes.get("/all", getAllTransactionController);
+transactionRoutes.get("/:id", getTransactionByIdController);
+transactionRoutes.put("/duplicate/:id", duplicateTransactionController);
 
 export default transactionRoutes;
